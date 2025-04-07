9fcee32513c436cfc5fc02871d278085f66e8252
sanctionedpranav <pranav9728@gmail.com>
2025-04-07 22:11:31 +0530

    projects section done feedng the data has left

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -68,7 +68,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "pranavs0208@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
